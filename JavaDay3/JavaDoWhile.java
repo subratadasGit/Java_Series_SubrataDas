876418ee76cb6142d0cc941b2b39c98ff2a2f3f0
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-03-08 18:23:43 +0000

    Do while

---
+++ JavaDay3/JavaDoWhile.java
@@ -0,0 +1,12 @@
+public class JavaDoWhile {
+    
+    public static void main(String[] args) {
+        // exit control loop
+        int i = 0 ;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while( i<5 ) ;
+    }
+}
