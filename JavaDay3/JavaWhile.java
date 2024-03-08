4b976c5cae54e397fe4bdc8f3643471e5c03992d
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-03-08 18:19:58 +0000

    While LOOP

---
+++ JavaDay3/JavaWhile.java
@@ -0,0 +1,15 @@
+class JavaWhile{
+
+    public static void main(String[] args) {
+        
+        int i = 0;
+
+        //While Loop
+        
+        while(i<5){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
