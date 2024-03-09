683f88fd00c93ca6d1d5fc980439448d3661da7f
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-03-09 16:19:15 +0530

    Add files via upload

---
+++ JavaDay3/javaFor.java
@@ -0,0 +1,13 @@
+public class javaFor {
+    
+    public static void main(String[] args) {
+        
+     for (int i = 0; i < 5; i++) {
+
+
+        System.err.println("Pronoy Da");
+        
+     }   
+    
+}
+}
